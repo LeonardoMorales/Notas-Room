00ac5fbbdef5e677a126bb761ee61fe4f6b838f2
Leonardo Morales <leonardo_moralesespinosa@live.com.mx>
2021-10-26 16:44:52 -0500

    AppDatabase configuration

--- app/src/main/java/dev/leonardom/notasroom/di/AppModule.kt
+++ app/src/main/java/dev/leonardom/notasroom/di/AppModule.kt
@@ -1,10 +1,15 @@
 package dev.leonardom.notasroom.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.leonardom.notasroom.R
+import dev.leonardom.notasroom.data.cache.AppDatabase
+import dev.leonardom.notasroom.data.cache.AppDatabase.Companion.DATABASE_NAME
+import dev.leonardom.notasroom.data.cache.note.NoteDao
 import dev.leonardom.notasroom.domain.repositories.NoteRepository
 import dev.leonardom.notasroom.presentation.note_detail.ColorSelectorAdapter
 import dev.leonardom.notasroom.presentation.note_list.NoteListAdapter
@@ -28,4 +33,30 @@ object AppModule {
     @Singleton
     fun provideNoteRepository() = NoteRepository()
 
+    @Provides
+    @Singleton
+    fun provideAppDatabase(app: Application): AppDatabase {
+        return Room
+            .databaseBuilder(app, AppDatabase::class.java, DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteDao(
+        db: AppDatabase
+    ): NoteDao {
+        return db.getNoteDao()
+    }
+
 }
+
+
+
+
+
+
+
+
+
