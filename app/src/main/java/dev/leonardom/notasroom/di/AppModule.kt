0149f33edde68c8e1f905c71cca2c770b866f13d
Leonardo Morales <leonardo_moralesespinosa@live.com.mx>
2021-11-10 16:19:40 -0600

    Migration to Jetpack Compose

--- app/src/main/java/dev/leonardom/notasroom/di/AppModule.kt
+++ app/src/main/java/dev/leonardom/notasroom/di/AppModule.kt
@@ -6,29 +6,16 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.leonardom.notasroom.R
 import dev.leonardom.notasroom.data.cache.AppDatabase
 import dev.leonardom.notasroom.data.cache.AppDatabase.Companion.DATABASE_NAME
 import dev.leonardom.notasroom.data.cache.note.NoteDao
 import dev.leonardom.notasroom.domain.repositories.NoteRepository
 import dev.leonardom.notasroom.domain.repositories.SettingsRepository
-import dev.leonardom.notasroom.presentation.note_detail.ColorSelectorAdapter
-import dev.leonardom.notasroom.presentation.note_list.NoteListAdapter
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Provides
-    @Singleton
-    fun provideNoteListAdapter() = NoteListAdapter()
-
-    @Provides
-    @Singleton
-    fun provideColorSelectorAdapter() = ColorSelectorAdapter(
-        listOf(R.color.app_bg_color, R.color.red001, R.color.pink002, R.color.orange003, R.color.orange004, R.color.yellow005, R.color.green006, R.color.green007, R.color.green008, R.color.blue009, R.color.blue010, R.color.purple011)
-    )
 
     @Provides
     @Singleton
--- app/src/main/java/dev/leonardom/notasroom/presentation/MainActivity.kt
+++ app/src/main/java/dev/leonardom/notasroom/presentation/MainActivity.kt
@@ -1,18 +1,19 @@
 package dev.leonardom.notasroom.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
 import dagger.hilt.android.AndroidEntryPoint
-import dev.leonardom.notasroom.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            Text("Nuevo texto creado con Jetpack Compose")
+        }
     }
 }
